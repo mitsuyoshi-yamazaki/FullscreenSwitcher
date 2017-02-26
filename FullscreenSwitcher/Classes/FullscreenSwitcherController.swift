015929261efdc96e1b2fbbd1852ec5e9600a05ca
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2017-02-26 15:36:06 +0900

    add framework target

---
+++ FullscreenSwitcher/Classes/FullscreenSwitcherController.swift
@@ -0,0 +1,35 @@
+//
+//  FullscreenSwitcherController.swift
+//  FullscreenSwitcher
+//
+//  Created by Yamazaki Mitsuyoshi on 2017/02/26.
+//  Copyright © 2017 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import UIKit
+
+class FullscreenSwitcherController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FullscreenSwitcher/FullscreenSwitcher.h
@@ -0,0 +1,19 @@
+//
+//  FullscreenSwitcher.h
+//  FullscreenSwitcher
+//
+//  Created by Yamazaki Mitsuyoshi on 2017/02/26.
+//  Copyright © 2017 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FullscreenSwitcher.
+FOUNDATION_EXPORT double FullscreenSwitcherVersionNumber;
+
+//! Project version string for FullscreenSwitcher.
+FOUNDATION_EXPORT const unsigned char FullscreenSwitcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FullscreenSwitcher/PublicHeader.h>
+
+
