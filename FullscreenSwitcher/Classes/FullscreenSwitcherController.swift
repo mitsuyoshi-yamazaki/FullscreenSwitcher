8264f67366ce8fb42b0757c43b6cf7943f95c74a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2017-02-26 16:15:06 +0900

    add basic api

--- FullscreenSwitcher/Classes/FullscreenSwitcherController.swift
+++ FullscreenSwitcher/Classes/FullscreenSwitcherController.swift
@@ -8,28 +8,68 @@
 
 import UIKit
 
-class FullscreenSwitcherController: UIViewController {
+public class FullscreenSwitcherController: UIViewController {
 
-    override func viewDidLoad() {
+    // MARK: - Accessor
+    // MARK: Public
+    public var fullscreenViewController: UIViewController! {
+        didSet {
+            addChildViewController(fullscreenViewController)
+            fullscreenViewController.didMove(toParentViewController: self)
+        }
+    }
+    public var contentViewController: UIViewController! {
+        didSet {
+            addChildViewController(contentViewController)
+            contentViewController.didMove(toParentViewController: self)
+        }
+    }
+    public var switchButton: UIButton! {
+        didSet {
+
+        }
+    }
+
+    // MARK: Private
+    private let fullscreenView: UIView = {
+
+        let view = UIView()
+        view.backgroundColor = UIColor.clear
+        view.autoresizingMask = [ .flexibleWidth, .flexibleHeight ]
+
+        return view
+    }()
+
+    private let contentView: UIView = {
+
+        let view = UIView()
+        view.backgroundColor = UIColor.clear
+        view.autoresizingMask = [ .flexibleWidth, .flexibleHeight ]
+
+        return view
+    }()
+
+    // MARK: - Lifecycle
+    override public func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        assert(fullscreenViewController != nil, "fullscreenViewController cannot be nil")
+        assert(contentViewController != nil, "contentViewController cannot be nil")
+
+        fullscreenView.frame = view.bounds
+        view.addSubview(fullscreenView)
+
+        contentView.frame = view.bounds
+        view.addSubview(contentView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        fullscreenView.addSubview(fullscreenViewController.view)
+        fullscreenViewController.view.frame = view.bounds
+
+        contentView.addSubview(contentViewController.view)
+        contentViewController.view.frame = view.bounds
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
